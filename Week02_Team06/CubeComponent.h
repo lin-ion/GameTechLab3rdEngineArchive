7c4228173e26a2f1631f607e8b0bfc57c8d3a3a7
KimKihoon <leonardkim1209@gmail.com>
2026-03-16 10:52:19 +0900

    save: 세이브포인트 저장

--- Week02_Team06/CubeComponent.h
+++ Week02_Team06/CubeComponent.h
@@ -17,7 +17,7 @@ public:
     void SetMesh(UMesh* InMesh) { MeshAsset = InMesh; }
 
     virtual void Render(ID3D11DeviceContext& DevcieContext) override;
-
+      
 private:
     UMesh* MeshAsset = nullptr;
     bool bIsMeshOwner = false; // AddMesh로 직접 만들었을 때 메모리 해제를 위한 플래그
