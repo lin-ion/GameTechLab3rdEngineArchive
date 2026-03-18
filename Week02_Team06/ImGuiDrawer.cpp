0da95a2964a3dbb0590133631066ae680e417a10
남윤지 <rora000924@gmail.com>
2026-03-18 14:05:29 +0900

    Fix: SelectedActor ImGui

--- Week02_Team06/ImGuiDrawer.cpp
+++ Week02_Team06/ImGuiDrawer.cpp
@@ -150,7 +150,7 @@ void UImGuiDrawer::DrawPrimitiveDataPanel()
 {
 	ImGui::Begin("Jungle Property Window");
 
-	//SelectedTarget = World->GetPickedActor();
+	SelectedTarget = World->SelectedActor;
 
 	if (SelectedTarget)
 	{
