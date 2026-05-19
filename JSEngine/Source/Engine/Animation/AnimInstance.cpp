d1a3776fb7d191c4e83e75b44a4bf703121eb0ca
Minjun Jang <jmj0ok@gmail.com>
2026-05-19 19:46:04 +0900

    feat: add anim instance variable map

--- JSEngine/Source/Engine/Animation/AnimInstance.cpp
+++ JSEngine/Source/Engine/Animation/AnimInstance.cpp
@@ -2,6 +2,14 @@
 
 DEFINE_CLASS(UAnimInstance, UObject)
 
+namespace
+{
+    bool IsValidAnimVariableName(const FName& Name)
+    {
+        return Name.IsValid() && Name != FName::None;
+    }
+}
+
 void UAnimInstance::Initialize(USkeletalMeshComponent* InOwningComponent)
 {
     OwningComponent = InOwningComponent;
@@ -46,4 +54,76 @@ USkeletalMeshComponent* UAnimInstance::GetOwningComponent() const
 USkeletalMeshComponent* UAnimInstance::GetSkelMeshComponent() const
 {
     return OwningComponent;
+}
+
+void UAnimInstance::SetAnimVariableFloat(const FName& Name, float Value)
+{
+    if (!IsValidAnimVariableName(Name))
+    {
+        return;
+    }
+
+    FloatVariables[Name] = Value;
+}
+
+bool UAnimInstance::GetAnimVariableFloat(const FName& Name, float& OutValue) const
+{
+    if (!IsValidAnimVariableName(Name))
+    {
+        return false;
+    }
+
+    const auto It = FloatVariables.find(Name);
+    if (It == FloatVariables.end())
+    {
+        return false;
+    }
+
+    OutValue = It->second;
+    return true;
+}
+
+float UAnimInstance::GetAnimVariableFloatOrDefault(const FName& Name, float DefaultValue) const
+{
+    float Value = DefaultValue;
+    return GetAnimVariableFloat(Name, Value) ? Value : DefaultValue;
+}
+
+void UAnimInstance::SetAnimVariableBool(const FName& Name, bool Value)
+{
+    if (!IsValidAnimVariableName(Name))
+    {
+        return;
+    }
+
+    BoolVariables[Name] = Value;
+}
+
+bool UAnimInstance::GetAnimVariableBool(const FName& Name, bool& OutValue) const
+{
+    if (!IsValidAnimVariableName(Name))
+    {
+        return false;
+    }
+
+    const auto It = BoolVariables.find(Name);
+    if (It == BoolVariables.end())
+    {
+        return false;
+    }
+
+    OutValue = It->second;
+    return true;
+}
+
+bool UAnimInstance::GetAnimVariableBoolOrDefault(const FName& Name, bool DefaultValue) const
+{
+    bool Value = DefaultValue;
+    return GetAnimVariableBool(Name, Value) ? Value : DefaultValue;
+}
+
+void UAnimInstance::ClearAnimVariables()
+{
+    FloatVariables.clear();
+    BoolVariables.clear();
 }
