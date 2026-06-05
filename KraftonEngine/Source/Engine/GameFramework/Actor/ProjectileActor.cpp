c95c8167eb97e7942e4be9ec3ee7c87707df585a
HanYoungSeo <108516412+persona6whenrelease@users.noreply.github.com>
2026-06-06 03:35:07 +0900

    particle actor wip

---
+++ KraftonEngine/Source/Engine/GameFramework/Actor/ProjectileActor.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include "ProjectileActor.h"
+
+#include "Engine/Component/Primitive/StaticMeshComponent.h"
+#include "Engine/Component/Particle/ParticleSystemComponent.h"
+#include "Particle/ParticleSystem.h"
+void AProjectileActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AProjectileActor::PostDuplicate()
+{
+	Super::PostDuplicate();
+
+}
+
+
+// 	SpringArm->AttachToComponent(CapsuleComponent);
+// root 가 capsule component
+void AProjectileActor::InitDefaultComponents()
+{
+	StaticMeshComponent = AddComponent<UStaticMeshComponent>();
+	ParticleSystemComponent = AddComponent<UParticleSystemComponent>();
+
+	SetRootComponent(StaticMeshComponent);
+	ParticleSystemComponent->AttachToComponent(StaticMeshComponent);
+
+
+	UParticleSystem* PS = UObjectManager::Get().CreateObject<UParticleSystem>();
+	// BuildDemoTemplate(PS);
+
+	ParticleSystemComponent->SetTemplate(PS);
+	ParticleSystemComponent->Activate(false);
+
+}
--- KraftonEngine/Source/Engine/GameFramework/Actor/ProjectileActor.h
+++ KraftonEngine/Source/Engine/GameFramework/Actor/ProjectileActor.h
@@ -1,1 +1,32 @@
-#pragma once
+﻿#pragma once
+#include "GameFramework/AActor.h"
+#include "Object/Ptr/WeakObjectPtr.h"
+
+class UParticleSystemComponent;
+class UStaticMeshComponent;
+#include "Source/Engine/GameFramework/Actor/ProjectileActor.generated.h"
+
+
+UCLASS()
+class AProjectileActor : public AActor
+{
+public:
+	GENERATED_BODY()
+	AProjectileActor() = default;
+	
+	void InitDefaultComponents();
+	void PostDuplicate() override;
+	void BeginPlay() override;
+
+	UFUNCTION(Pure, Category = "Actor|Components")
+	UStaticMeshComponent* GetStaticComponent() const { return StaticMeshComponent; }
+	
+	UFUNCTION(Pure, Category = "Actor|Components")
+	UParticleSystemComponent* GetParticleSystemComponent() const { return ParticleSystemComponent; }
+
+
+private:
+	TWeakObjectPtr<UStaticMeshComponent> StaticMeshComponent = nullptr;
+	TWeakObjectPtr<UParticleSystemComponent> ParticleSystemComponent = nullptr;
+
+};
